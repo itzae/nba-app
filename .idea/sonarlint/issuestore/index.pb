05327493d5f7aa8327ebc44dc90b0704dec9083e
Itzae <itzaeg@gmail.com>
2021-01-20 16:26:59 -0600

    TA: #1 Kotlin DSL has been configured to have a better handling of project dependencies

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+@
+local.properties,0\7\0712df971a99ac4d2fccb8e0fb19f377f3374cca
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+d
+4app/src/main/java/com/itgonca/nbaapp/MainActivity.kt,c\5\c5f085c3a30b4f174889afff55f4ef9446e8d6a5
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,23 @@
+import sun.misc.Version
+
+object Versions {
+    const val kotlin_version = "1.3.72"
+    const val core_ktx_version = "1.3.2"
+    const val appcompat_version = "1.2.0"
+    const val material_design_version = "1.2.1"
+    const val constraintlayout_version = "2.0.4"
+    const val junit_version = "4.13.1"
+    const val ext_unit_version = "1.1.2"
+    const val espresso_core_version = "3.3.0"
+}
+
+object Dependencies {
+    const val kotlin_lib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
+    const val core_ktx_lib = "androidx.core:core-ktx:${Versions.core_ktx_version}"
+    const val appcompat_lib = "androidx.appcompat:appcompat:${Versions.appcompat_version}"
+    const val material_design_lib = "com.google.android.material:material:${Versions.material_design_version}"
+    const val constraintlayout_lib = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout_version}"
+    const val junit_lib = "junit:junit:${Versions.junit_version}"
+    const val ext_junit_lib = "androidx.test.ext:junit:${Versions.ext_unit_version}"
+    const val espresso_core_lib = "androidx.test.espresso:espresso-core:${Versions.espresso_core_version}"
+}
