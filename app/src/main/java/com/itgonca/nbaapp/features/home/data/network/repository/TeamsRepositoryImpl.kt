8d814c9109e76257daaf4a56cbae4834caf6a149
Itzae <itzaeg@gmail.com>
2021-05-12 18:13:51 -0500

    TA: #10 Remove API_KEY

--- app/src/main/java/com/itgonca/nbaapp/features/home/data/network/repository/TeamsRepositoryImpl.kt
+++ app/src/main/java/com/itgonca/nbaapp/features/home/data/network/repository/TeamsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.itgonca.nbaapp.features.home.data.network.repository
 
+import com.itgonca.nbaapp.BuildConfig
 import com.itgonca.nbaapp.features.home.data.network.api.TeamsApi
 import com.itgonca.nbaapp.features.home.data.network.model.TeamsResponse
 import javax.inject.Inject
@@ -7,7 +8,7 @@ import javax.inject.Inject
 class TeamsRepositoryImpl @Inject constructor(val remoteTeams: TeamsApi) : TeamsRepository {
     override suspend fun getTeamsPerConference(conference: String): TeamsResponse =
         remoteTeams.getTeamsPerConference(
-            "a6838d14abmsh7639ba8abedb803p1f3c94jsn86d70d44c12d",
+            BuildConfig.API_KEY,
             conference
         )
 }
